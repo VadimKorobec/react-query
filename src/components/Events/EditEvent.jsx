e8f9f9a96af56789ada1bc5948a9fc85eca72d59
Vadim <_s_e_t__@ukr.net>
2024-12-18 15:10:45 +0200

    add some code

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -16,7 +16,29 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueriesData(["events", params.id], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    }
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
@@ -25,34 +47,44 @@ export default function EditEvent() {
   let content;
 
   if (isPending) {
-    content = <LoadingIndicator />;
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
   }
 
   if (isError) {
     content = (
-      <ErrorBlock
-        title="failed fatch event"
-        message={
-          error.info?.message || "Failed fatch event. Please try again later."
-        }
-      />
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            "Failed to load event. Please try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
     );
   }
 
   if (data) {
     content = (
-      <Modal onClose={handleClose}>
-        <EventForm inputData={data} onSubmit={handleSubmit}>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Update
-          </button>
-        </EventForm>
-      </Modal>
+      <EventForm inputData={data} onSubmit={handleSubmit}>
+        <Link to="../" className="button-text">
+          Cancel
+        </Link>
+        <button type="submit" className="button">
+          Update
+        </button>
+      </EventForm>
     );
   }
 
-  return { content };
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
